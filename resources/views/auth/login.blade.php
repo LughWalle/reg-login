886b1e376df5b5fb9e2bf9f959da1638ce281872
LughWalle <civalfran4@outlook.com>
2020-03-11 10:05:24 -0300

    novo test

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,105 +1,73 @@
 @extends('layouts.app')
 
 @section('content')
-            <!-- Intro -->
-            <section class="site-section site-section-light site-section-top themed-background-dark">
-                <div class="container">
-                    <h1 class="text-center animation-slideDown"><i class="fa fa-arrow-right"></i> <strong>Log In</strong></h1>
-                    <h2 class="h3 text-center animation-slideUp">Connect to your dashboard!</h2>
-                </div>
-            </section>
-            <!-- END Intro -->
-                        
-            <!-- Log In -->
-            <section class="site-content site-section">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-sm-6 col-sm-offset-3 col-lg-4 col-lg-offset-4 site-block">
-                            <!-- Log In Form -->
-                            <form action="{{ route('login') }}" method="POST" id="form-log-in" class="form-horizontal">
-                                @csrf
-                                <div class="form-group">
-                                    <div class="col-xs-12">
-                                        <div class="input-group">
-                                            <span class="input-group-addon"><i class="gi gi-envelope"></i></span>
-                                            <input type="email" id="login-email" name="login-email" class="form-control input-lg @error('email') is-invalid @enderror" value="{{ old('email') }}" placeholder="Email" required autocomplete="email" autofocus>
-                                            @error('email')
-                                                <span class="invalid-feedback" role="alert">
-                                                    <strong>{{ $message }}</strong>
-                                                </span>
-                                            @enderror
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <div class="col-xs-12">
-                                        <div class="input-group">
-                                            <span class="input-group-addon"><i class="gi gi-asterisk"></i></span>
-                                            <input type="password" id="login-password" name="login-password" class="form-control input-lg @error('password') is-invalid @enderror" placeholder="Password" required autocomplete="current-password">
-                                            @error('password')
-                                                <span class="invalid-feedback" role="alert">
-                                                    <strong>{{ $message }}</strong>
-                                                </span>
-                                            @enderror
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="form-group form-actions">
-                                    <div class="col-xs-6">
-                                        <label class="switch switch-primary">
-                                            <input type="checkbox" id="login-remember-me" name="login-remember-me" {{ old('remember') ? 'checked' : '' }}><span></span>
-                                        </label>
-                                        <small>Remember me</small>
-                                    </div>
-                                    <div class="col-xs-6 text-right">
-                                        <button type="submit" class="btn btn-sm btn-primary"><i class="fa fa-arrow-right"></i> Log In</button>
-                                    </div>
-                                </div>
-                                <div class="form-group">
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Login') }}</div>
 
-                                </div>
-                            </form>
-                            <div class="text-center">
-                                <a href="javascript:void(0)"><small>Forgot password?</small></a>
+                <div class="card-body">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
-                            <!-- END Log In Form -->
                         </div>
-                    </div>
-                    <hr>
-                </div>
-            </section>
-            <!-- END Log In -->
 
-            <!-- Support Links -->
-            <section class="site-content site-section">
-                <div class="container">
-                    <div class="row row-items text-center">
-                        <div class="col-sm-3 animation-fadeIn">
-                            <a href="javascript:void(0)" class="circle themed-background">
-                                <i class="gi gi-life_preserver"></i>
-                            </a>
-                            <h4>Open a <strong>ticket</strong></h4>
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
                         </div>
-                        <div class="col-sm-3 animation-fadeIn">
-                            <a href="javascript:void(0)" class="circle themed-background">
-                                <i class="gi gi-envelope"></i>
-                            </a>
-                            <h4><strong>Email</strong> Us</h4>
+
+                        <div class="form-group row">
+                            <div class="col-md-6 offset-md-4">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label>
+                                </div>
+                            </div>
                         </div>
-                        <div class="col-sm-3 animation-fadeIn">
-                            <a href="javascript:void(0)" class="circle themed-background">
-                                <i class="fa fa-comments"></i>
-                            </a>
-                            <h4><strong>Chat</strong> Live</h4>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                                </button>
+
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                @endif
+                            </div>
                         </div>
-                        <div class="col-sm-3 animation-fadeIn">
-                            <a href="javascript:void(0)" class="circle themed-background">
-                                <i class="fa fa-twitter"></i>
-                            </a>
-                            <h4><strong>Tweet</strong> Us</h4>
-                        </div>
-                    </div>
+                    </form>
                 </div>
-            </section>
-            <!-- END Support Links -->
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
